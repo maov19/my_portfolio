52ec7910c0697c97bba2e2cab5c7398ce2098388
mossa <martin.ossa.8@gmail.com>
2023-01-30 20:47:46 +0100

    Fix linter setup

--- app.js
+++ app.js
@@ -1,18 +1,14 @@
-let icon = document.getElementById('icon');
-let menu = document.getElementById('menuAll');
+const icon = document.getElementById('icon');
+const menu = document.getElementById('menuAll');
 
-icon.onclick = function () {
-    icon.classList.toggle('is-active');
-    menu.classList.toggle('is-active');
-}
+icon.onclick = function a() {
+  icon.classList.toggle('is-active');
+  menu.classList.toggle('is-active');
+};
 
-document.onclick = function(clickEvent) {
-    if(clickEvent.target.id !== 'menuAll' && clickEvent.target.id !== 'icon') {
-        icon.classList.remove('is-active');
-        menu.classList.remove('is-active');
-    }
-}
-
-
-
-
+document.onclick = function b(clickEvent) {
+  if (clickEvent.target.id !== 'menuAll' && clickEvent.target.id !== 'icon') {
+    icon.classList.remove('is-active');
+    menu.classList.remove('is-active');
+  }
+};
