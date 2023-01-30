e5a0642e44daf671ea9db41ad312ef2409f964bf
mossa <martin.ossa.8@gmail.com>
2023-01-30 19:50:05 +0100

    Fix navbar navigation setup

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
-let menuBtn = document.querySelector('.icon');
-let menuMobile = document.querySelector('.menuAll')
+let icon = document.getElementById('icon');
+let menu = document.getElementById('menuAll');
 
-menuBtn.addEventListener('click', function () {
-    menuBtn.classList.toggle('is-active');
-    menuMobile.classList.toggle('is-active');
+icon.onclick = function () {
+    icon.classList.toggle('is-active');
+    menu.classList.toggle('is-active');
+}
 
-});
+document.onclick = function(clickEvent) {
+    if(clickEvent.target.id !== 'menuAll' && clickEvent.target.id !== 'icon') {
+        icon.classList.remove('is-active');
+        menu.classList.remove('is-active');
+    }
+}
 
-let menu = document.getElementById('icon');
 
-let menuAll = document.getElementById('menuAll')
-menu.addEventListener('click', function() {
-    menu.style.display = 'none';
-    Object.assign(menuAll.style, styles);
-})
+
 
