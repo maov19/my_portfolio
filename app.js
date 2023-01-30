2a52aba016c023add096a2d72761160405e0d9a5
mossa <martin.ossa.8@gmail.com>
2023-01-30 15:01:52 +0100

    Add menu to hamburger icon and its animation

--- app.js
+++ app.js
@@ -1,18 +1,17 @@
+let menuBtn = document.querySelector('.icon');
+let menuMobile = document.querySelector('.menuAll')
+
+menuBtn.addEventListener('click', function () {
+    menuBtn.classList.toggle('is-active');
+    menuMobile.classList.toggle('is-active');
+
+});
 
 let menu = document.getElementById('icon');
-let image = document.getElementById('hamburger');
-
-let styles = {
-    display: 'block',
-    border: '2px solid blue',
-    position: 'relative',
-    top: '25px',
-    padding: '10px',
-    'font-family': 'poppins',
-}
 
 let menuAll = document.getElementById('menuAll')
 menu.addEventListener('click', function() {
-    menu.style.display = 'none'
-    Object.assign(menuAll.style, styles)
+    menu.style.display = 'none';
+    Object.assign(menuAll.style, styles);
 })
+
