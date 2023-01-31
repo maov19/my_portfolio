e7858dc695d1792e55d1118897ae141cc76fef17
mossa <martin.ossa.8@gmail.com>
2023-01-31 12:14:17 +0100

    Add popups to see project buttons

--- app.js
+++ app.js
@@ -1,14 +1,60 @@
+// navigation bar js
 const icon = document.getElementById('icon');
 const menu = document.getElementById('menuAll');
 
-icon.onclick = function a() {
+icon.onclick = function openMenu() {
   icon.classList.toggle('is-active');
   menu.classList.toggle('is-active');
 };
 
-document.onclick = function b(clickEvent) {
-  if (clickEvent.target.id !== 'menuAll' && clickEvent.target.id !== 'icon') {
+document.onclick = function closeMenu(clickEvent) {
+  if (clickEvent.target.id !== 'menuAll' && clickEvent.target.id !== 'icon') { //when something is clicked in the menu, that is not the open or close button
     icon.classList.remove('is-active');
     menu.classList.remove('is-active');
   }
 };
+
+// popup js
+
+const openModalButtons = document.querySelectorAll('[data-modal-target]');
+const closeModalButtons = document.querySelectorAll('[data-close-button]');
+const overlay = document.getElementById('overlay');
+
+
+// open button function
+function openModal (modal) {
+  if (modal == null) return
+  modal.classList.add('active');
+  overlay.classList.add('active');
+}
+
+// close button function
+function closeModal (modal) {
+  if (modal == null) return
+  modal.classList.remove('active');
+  overlay.classList.remove('active');
+}
+
+// assign open funcion to button
+openModalButtons.forEach (button => {
+  button.addEventListener('click', () => {
+    const modal = document.querySelector(button.dataset.modalTarget); //querySelector works based on modalTarget of html
+    openModal(modal); //open button
+  })
+})
+
+// assign closing function to button
+closeModalButtons.forEach (button => {
+  button.addEventListener('click', () => {
+    const modal = button.closest('.modal') //checks if the closest parent is in modal class
+    closeModal(modal); //close button
+  });
+});
+
+// assign closing function when click outside
+overlay.addEventListener('click', () => {
+  const modals = document.querySelectorAll('.modal.active');
+  modals.forEach(modal => {
+    closeModal(modal);
+  }); 
+});
---
+++ steps_popup.md
@@ -0,0 +1,23 @@
+create a popup and and exit button
+and assign it to each button
+loop over content
+display content iterated on each project
+
+
+
+
+Project requirements
+
+You should implement the following interactions:
+
+- When the user clicks (or taps) the button to check project details, the popup with details about the project appears. - DONE
+- When the user clicks (or taps) the close (X) button, the popup disappears. - DONE
+• In order to associate each project with the popup window details, you must refactor the project section:
+
+a. You need to use a JavaScript array to store all of the information for all projects.
+b. For each project you need to store the following pieces of data in a JavaScript object, at a minimum: name, description, featured image, technologies, link to live version, link to source.
+c. You need to update the main page so that the projects section is created dynamically using the information stored in that JavaScript object. Remember, all of the HTML in that section is created when the page loads.
+d. After that, you could implement the popup window.
+e. You should implement popups for both mobile and desktop screen sizes:
+
+https://github.com/microverseinc/curriculum-html-css/blob/main/portfolio/8_popup.md
