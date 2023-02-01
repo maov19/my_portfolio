3eb39801c21529e6d951f5384818a407a33b3cdb
mossa <martin.ossa.8@gmail.com>
2023-02-01 19:54:10 +0100

    Add function for form validation

--- app.js
+++ app.js
@@ -280,3 +280,53 @@ for (let i = 0; i < portfolios.length; i += 1) {
     popupModal.style.display = 'none';
   });
 }
+
+//form validation
+
+
+// The content of the email field has to be in lower case.
+// when user submits
+// If the validation is OK, the form is sent.
+// If the validation is not OK, you show an error message to the user near the submit button informing them of the error and the form is not sent.
+
+//check if cell has value
+function hasValue(input, message) {
+	if (input.value.trim() === "") {
+		return showError(input, message);
+	}
+	return showSuccess(input);
+}
+// show a message with a type of the input
+function validateEmail(input, requiredMsg, invalidMsg) {
+	// check if the value is not empty
+	if (!hasValue(input, requiredMsg)) {
+		return false;
+	}
+	// validate email format
+	const emailRegex =
+		/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+	const email = input.value.trim();
+	if (!emailRegex.test(email)) {
+		return showError(input, invalidMsg);
+	}
+	return true;
+}
+
+const form = document.querySelector("#contact");
+
+const NAME_REQUIRED = "Please enter your name";
+const EMAIL_REQUIRED = "Please enter your email";
+const EMAIL_INVALID = "Please enter a correct email address format";
+
+form.addEventListener("submit", function (event) {
+	// stop form submission
+	event.preventDefault();
+
+	// validate the form
+	let emailValid = validateEmail(form.elements["email"], EMAIL_REQUIRED, EMAIL_INVALID);
+	// if valid, submit the form.
+	if (emailValid) {
+		alert("Demo only. No form was posted.");
+	}
+});
